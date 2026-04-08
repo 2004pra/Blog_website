caf981fb2843ad00aab5a06c60ee425243f512f9
prashant <prashant37364@gmail.com>
2026-04-08 23:19:20 +0530

    final loader file added

--- Backend/hello.py
+++ Backend/hello.py
@@ -112,6 +112,6 @@ if __name__ == '__main__':
 
 from flask import send_file
 
-@app.route('loaderio-008b1e2cedea14cabab1f2b077e3df12.txt')
+@app.route('/loaderio-008b1e2cedea14cabab1f2b077e3df12.txt')
 def loaderio_verify():
     return send_file('loaderio-008b1e2cedea14cabab1f2b077e3df12.txt')
